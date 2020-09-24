b9a99b8c7f0356f35b5aee690c192982bedc86bc
Ridz Chandra <ridzchandra9@gmail.com>
2020-09-24 20:13:36 +1000

    only input

---
+++ input.go
@@ -0,0 +1,15 @@
+package chandrikio
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+// Input ... Python like console input
+func Input(prompt string) string {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print(prompt)
+	scanner.Scan()
+	return scanner.Text()
+}
